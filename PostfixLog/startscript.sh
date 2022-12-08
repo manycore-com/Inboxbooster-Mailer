de30b07459fa3192d6f324a8007a055af68d6f5e
ehsmeng <marcus@manycore.io>
2022-12-08 13:38:03 -0800

    proper service definitions for receiver

---
+++ PostfixLog/startscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp /configs/mailname /etc/mailname
+service postfix start
+cat /etc/mailname
+sleep 2
+ps -ef |grep postfix
+python3 main.py \
+  --global-config-file=/configs/manycore-mail-global.yaml \
+  --customer-config-file=/configs/manycore-mail-customer.yaml
