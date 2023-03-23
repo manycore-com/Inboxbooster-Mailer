f502dd10e5a194e95a0afaf587f18fdca88b6313
ehsmeng <marcus@manycore.io>
2023-03-22 20:59:47 -0700

    fix corner case where mailer is configured oddly

--- Redis/shutdown.py
+++ Redis/shutdown.py
@@ -31,31 +31,34 @@ if __name__ == "__main__":
     os.system("redis-cli shutdown save")
 
     if os.path.isfile("/var/lib/redis/dump.rdb"):
-        if "common-config" not in customer_config or "object-storage" not in customer_config["common-config"]:
+        if "common-config" not in customer_config:
             logging.warning(args.customer_config_file + " is missing configuration for object storage. Existing data will be lost.")
         else:
-            try:
-                conf_obj = customer_config["common-config"]["object-storage"]
-                assert conf_obj["type"] == "s3"
-                aws_auth = {
-                    'region_name': conf_obj["region"],
-                    'aws_access_key_id': conf_obj["access-key-id"],
-                    'aws_secret_access_key': conf_obj["secret-access-key"],
-                }
+            if customer_config["common-config"] is None or "object-storage" not in customer_config["common-config"]:
+                logging.warning(args.customer_config_file + " is missing configuration for object storage. Existing data will be lost.")
+            else:
+                try:
+                    conf_obj = customer_config["common-config"]["object-storage"]
+                    assert conf_obj["type"] == "s3"
+                    aws_auth = {
+                        'region_name': conf_obj["region"],
+                        'aws_access_key_id': conf_obj["access-key-id"],
+                        'aws_secret_access_key': conf_obj["secret-access-key"],
+                    }
 
-                session = boto3.session.Session(**aws_auth)
-                s3 = session.client(
-                    "s3",
-                    endpoint_url=conf_obj["endpoint-url"]
-                )
-                s3.upload_file(
-                    Bucket=conf_obj["bucket"],
-                    Key="dump.rdb",
-                    Filename=Path("/var/lib/redis/dump.rdb").as_posix(),
-                    ExtraArgs={},
-                )
-            except Exception as e:
-                logging.error("Failed uploading dump.rdb: " + str(e))
+                    session = boto3.session.Session(**aws_auth)
+                    s3 = session.client(
+                        "s3",
+                        endpoint_url=conf_obj["endpoint-url"]
+                    )
+                    s3.upload_file(
+                        Bucket=conf_obj["bucket"],
+                        Key="dump.rdb",
+                        Filename=Path("/var/lib/redis/dump.rdb").as_posix(),
+                        ExtraArgs={},
+                    )
+                except Exception as e:
+                    logging.error("Failed uploading dump.rdb: " + str(e))
 
     # the dummy tail -f /dev/null
     os.system("pkill -f \"tail -F redis_dummy_tail\"")
